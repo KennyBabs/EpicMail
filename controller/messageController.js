f7aa96a23eb1271dd1ff564cb5d77d98a0e52e4c
Babalola kehinde <Babalolakehinde.p@gmail.com>
2019-03-10 21:36:02 +0100

    Add post email endpoint to pass the unit test

--- controller/messageController.js
+++ controller/messageController.js
@@ -8,8 +8,31 @@ class MessageController {
 		});
 
 		}
+		createMessages (req, res) {
 		
+			if(!req.body.subject || !req.body.message) {
+				return res.status(400).send({
+					status : 400,
+					error : 'fields required'
+				});
+			}
 		
+		const message = [{
+			id :req.body.id,
+			createdOn: Date.now(),
+			subject: req.body.subject,
+		  message: req.body.message,
+		  parentMessageId: req.body.parentMessageId,
+		  status: req.body.status,
+		}];
+
+		epicMail['Message'].push(message);	
+		 res.status(200).send({
+			status : 200,
+			data: message
+		});
+	}
+
 }
 
 const messageController = new MessageController();
