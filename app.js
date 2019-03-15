22c485c1f43fc44bb1ee84f52f7ae0cee5241e49
Babalola kehinde <Babalolakehinde.p@gmail.com>
2019-03-15 06:55:32 +0100

    deploy app on heroku

--- app.js
+++ app.js
@@ -8,6 +8,17 @@ const app = express();
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api/v1', router);
 
+app.get('/', (req, res) => res.status(200).send({ 
+	message: 'WELCOME' 
+}));
+
+app.get('*', (req, res) => {
+	res.status(404).send({
+	  status: 404,
+	  error: 'This url does not exist',
+	});
+  });
+
 const port = process.env.PORT || 5000;
 
 const server = app.listen(port, () => {
