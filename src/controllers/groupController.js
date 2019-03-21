e18320d0a6ffcad72dc633ab5cbe3cb521fb609d
Babalola kehinde <Babalolakehinde.p@gmail.com>
2019-03-21 13:32:41 +0100

    feat(user): implement user delete group
    - define endpoint and methods for delete group
    - define delete group route
    - query database to delete a group
    
    [Delivers 164794915]

--- src/controllers/groupController.js
+++ src/controllers/groupController.js
@@ -27,6 +27,48 @@ class GroupController {
       message: 'Group created successfully',
     });
   }
+
+  static async deleteAGroup(req, res) {
+
+    let groupOutput = [];
+    let deleteUserGroupOutput = [];
+    let deleteGroupOutput = [];
+    const fetchGroup = 'SELECT * FROM groups WHERE id=$1 AND created_by=$2';
+    const deleteUserGroup = 'DELETE FROM user_group WHERE group_id=$1 returning *';
+    const deleteGroup = 'DELETE FROM groups WHERE id=$1 AND created_by=$2 returning *';
+
+    try {
+      const { rows } = await db.query(fetchGroup, [req.params.id, req.user.id]);
+      groupOutput = rows[0];
+      if (!groupOutput) {
+        return res.status(404).send({ message: 'Group not Found' });
+      }
+    } catch (e) {
+      return res.status(500).send({ message: 'request not granted' });
+    }
+
+    try {
+      const { rows } = await db.query(deleteUserGroup, [req.params.id]);
+      deleteUserGroupOutput = { rows };
+      if (!deleteUserGroupOutput) {
+        return res.status(404).send({ message: 'Access Denied' });
+      }
+    } catch (e) {
+      return res.status(400).send({ message: 'request not found' });
+    }
+
+    try {
+      const { rows } = await db.query(deleteGroup, [req.params.id, req.user.id]);
+      deleteGroupOutput = rows[0];
+      if (!deleteGroupOutput) {
+        return res.status(404).send({ message: 'Access Denied' });
+      }
+      return res.status(200).send({ message: 'group deleted' });
+    } catch (e) {
+      return res.status(500).send({ message: 'request not granted' });
+    }
+  }
+
 }
 
 export default GroupController;
