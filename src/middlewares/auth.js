28d20c5789e56291310e681a15a2238b205a4914
Babalola kehinde <Babalolakehinde.p@gmail.com>
2019-03-21 11:46:32 +0100

    add verify authentication middleware

---
+++ src/middlewares/auth.js
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+const Auth = {
+    verifyToken(req, res, next) {
+      const token = req.headers['x-access-token'];
+      if (!token) {
+        return res.status(401).send({ status: 401, error: 'Token is not provided' });
+      }
+      try {
+        const decoded = jwt.verify(token, process.env.MY_SECRET);
+        req.user = { id: decoded.userId };
+        return next();
+      } catch (error) {
+        return res.status(401).send({ status: 401, error: 'Token is invalid' });
+      }
+    },
+  };
+  
+  export default Auth;
